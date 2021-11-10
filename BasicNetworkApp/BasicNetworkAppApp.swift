24d03ae069050aeb36932a25615d758d5111bedf
tayyabmughal676 <tayyabmughal676@gmail.com>
2021-11-10 12:09:32 +0500

    Initial Commit

---
+++ BasicNetworkApp/BasicNetworkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicNetworkAppApp.swift
+//  BasicNetworkApp
+//
+//  Created by Thor on 10/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct BasicNetworkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
